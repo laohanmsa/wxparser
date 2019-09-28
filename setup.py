2a805966783b7479e3a93b0bf31905bab016de0f
an chen <laohan.msa@gmail.com>
2019-09-29 00:19:43 +0800

    update

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from distutils.core import setup
 setup(
   name='wxparser',
   packages=['wxparser'],
-  version='0.1',
+  version='0.4',
   license='MIT',
   description ='parser for wx article ',
   author='Mario',                   # Type in your name
   author_email ='laohan.msa@gmail.com',      # Type in your E-Mail
   url ='https://github.com/laohanmsa/wxparser.git',
-  download_url = 'https://github.com/laohanmsa/wxparser/archive/v0.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/laohanmsa/wxparser/archive/v0.4.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 0.4 test',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Code Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
